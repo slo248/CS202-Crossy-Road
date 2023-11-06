7fde051ee8bef2ed344bec05f3eecca958519b72
zHuskyz <nvk01052004@gmail.com>
2023-11-06 15:44:57 +0700

    add resources + write ResourceHolder

---
+++ include/ResourceHolder.hpp
@@ -0,0 +1,31 @@
+#ifndef RESOURCEHOLDER_HPP
+#define RESOURCEHOLDER_HPP
+
+#include <cassert>
+#include <map>
+#include <memory>
+#include <stdexcept>
+#include <string>
+
+template <typename Resource, typename Identifier>
+class ResourceHolder {
+   public:
+    void load(Identifier id, const std::string& filename);
+
+    template <typename Parameter>
+    void load(
+        Identifier id, const std::string& filename, const Parameter& secondParam
+    );
+
+    Resource& get(Identifier id);
+    const Resource& get(Identifier id) const;
+
+   private:
+    void insertResource(Identifier id, std::unique_ptr<Resource> resource);
+
+   private:
+    std::map<Identifier, std::unique_ptr<Resource>> mResourceMap;
+};
+
+#include "ResourceHolder.inl"
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,41 @@
-// This program draws green circle in the window.
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+#include "ResourceHolder.hpp"
+#include "ResourceIdentifiers.hpp"
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(640, 480), "Resources");
+    window.setFramerateLimit(20);
+
+    // Try to load resources
+    ResourceHolder<sf::Texture, Textures::ID> textures;
+    try {
+        textures.load(
+            Textures::bom_bats, "asset/s_vehicles/bombbats/Boom1.png"
+        );
+        textures.load(Textures::bee_boss, "asset/s_vehicles/bees/D_Attack.png");
+    } catch (std::runtime_error& e) {
+        std::cout << "Exception: " << e.what() << std::endl;
+        return 1;
+    }
+
+    // Access resources
+    sf::Sprite bombats(textures.get(Textures::bom_bats));
+    sf::Sprite beeboss(textures.get(Textures::bee_boss));
+    beeboss.setPosition(200.f, 200.f);
 
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) window.close();
+            if (event.type == sf::Event::KeyPressed ||
+                event.type == sf::Event::Closed)
+                return 0;
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(bombats);
+        window.draw(beeboss);
         window.display();
     }
-    return 0;
 }
