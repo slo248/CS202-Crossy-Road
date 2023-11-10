58fef748b620ea027721505e5fe0378c6c04bd5d
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-11-10 19:05:34 +0700

    add State and StateID

---
+++ include/State.hpp
@@ -0,0 +1,49 @@
+#ifndef STATE_HPP
+#define STATE_HPP
+
+#include <SFML/Graphics.hpp>
+#include <memory>
+
+#include "ResourceIdentifiers.hpp"
+#include "StateID.hpp"
+
+// Forward declaration
+class StateStack;
+class Player;
+//
+
+class State {
+   public:
+    typedef std::unique_ptr<State> Ptr;
+    struct Context {
+        Context(
+            sf::RenderWindow& window, TextureHolder& textures,
+            FontHolder& fonts, Player& player
+        );
+        sf::RenderWindow* window;
+        TextureHolder* textures;
+        FontHolder* fonts;
+        Player* player;
+    };
+
+   public:
+    State(StateStack& stack, Context context);
+    virtual ~State();
+
+    virtual void draw() = 0;
+    virtual bool update(sf::Time dt) = 0;
+    virtual bool handleEvent(const sf::Event& event) = 0;
+
+   protected:
+    void requestStackPush(States::ID stateID);
+    void requestStackPop();
+    void requestStackClear();
+
+    Context getContext() const;
+
+   private:
+    StateStack* mStack;
+    Context mContext;
+};
+
+#endif  // STATE_HPP
---
+++ include/StateID.hpp
@@ -0,0 +1,8 @@
+#ifndef STATEID_HPP
+#define STATEID_HPP
+
+namespace States {
+enum ID { None, Title, Menu, Setting, Game, Loading, Pause };
+}
+
+#endif  // STATEID_HPP
