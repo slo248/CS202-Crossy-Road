4b33c6ca56b840d1e3c3dad88b814537dcff21ed
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-11-14 19:42:10 +0700

    add stateID: win, lose, credit, ranking

--- include/StateID.hpp
+++ include/StateID.hpp
@@ -2,7 +2,19 @@
 #define STATEID_HPP
 
 namespace States {
-enum ID { None, Title, Menu, Setting, Game, Loading, Pause };
+enum ID {
+    None,
+    Title,
+    Menu,
+    Setting,
+    Game,
+    Loading,
+    Pause,
+    Win,
+    Lose,
+    Credit,
+    Ranking
+};
 }
 
 #endif  // STATEID_HPP
