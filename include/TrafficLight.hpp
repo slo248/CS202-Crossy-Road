92fd6e4d7537e66f549e7df222cc0e9d77c0be68
Lê Thanh Lâm <22125046@student.hcmus.edu.vn>
2023-11-20 20:37:11 +0700

    add TrafficLight animation

--- include/TrafficLight.hpp
+++ include/TrafficLight.hpp
@@ -10,22 +10,25 @@
 class TrafficLight : public SceneNode {
    public:
     enum Type { Field, Graveyard, Swamp, TypeCount };
+    enum Color { Red, Green, Blue };
 
+   public:
     TrafficLight(Type type, const TextureHolder& textures);
     virtual unsigned int getCategory() const override;
     virtual sf::FloatRect getBoundingRect() const override;
+    Color getColor();
 
    private:
     void updateCurrent(sf::Time dt, CommandQueue& commands) override;
     virtual void drawCurrent(sf::RenderTarget& target, sf::RenderStates states)
         const;
-    void updateMovementPattern(sf::Time dt);
 
    private:
-    sf::Sprite mSprite;
     Type mType;
-    int mColor;
-    sf::Time mTime;
+    sf::Time mElapsedTime;
+    sf::Sprite mSprite;
+    std::size_t mCurrentFrame;
+    std::size_t mNumFrames;
 };
 
 #endif  // TRAFFIC_LIGHT_HPP
--- src/Scene/TrafficLight/TrafficLight.cpp
+++ src/Scene/TrafficLight/TrafficLight.cpp
@@ -6,6 +6,11 @@
 #include "Category.hpp"
 #include "DataTables.hpp"
 #include "ResourceHolder.hpp"
+#include "Utility.hpp"
+
+#define GREEN_INTERVAL sf::seconds(1.5)
+#define YELLOW_INTERVAL sf::seconds(0.5)
+#define RED_INTERVAL sf::seconds(2.0)
 
 namespace {
 const std::vector<TrafficLightData> Table = initializeTrafficLightData();
@@ -13,23 +18,65 @@ const std::vector<TrafficLightData> Table = initializeTrafficLightData();
 
 TrafficLight::TrafficLight(Type type, const TextureHolder& textures)
     : mType(type),
-      mSprite(textures.get(Table[type].texture), Table[type].textureRect) {}
+      mSprite(textures.get(Table[type].texture), Table[type].textureRect),
+      mCurrentFrame(0) {}
 
 unsigned int TrafficLight::getCategory() const {
     return Category::TrafficLight;
 }
 
-sf::FloatRect TrafficLight::getBoundingRect() const {
-    return getWorldTransform().transformRect(mSprite.getGlobalBounds());
-    ;
+sf::FloatRect TrafficLight::getBoundingRect() const {}
+
+TrafficLight::Color TrafficLight::getColor() {
+    return static_cast<Color>(mCurrentFrame);
 }
 
-void TrafficLight::updateCurrent(sf::Time dt, CommandQueue& commands) {}
+void TrafficLight::updateCurrent(sf::Time dt, CommandQueue& commands) {
+    mElapsedTime += dt;
+
+    switch (mCurrentFrame) {
+        case 0: {
+            if (mElapsedTime >= GREEN_INTERVAL) {
+                mElapsedTime -= GREEN_INTERVAL;
+                mCurrentFrame = 6;
+                mNumFrames = 12;
+            }
+            break;
+        }
+
+        case 6: {
+            if (mElapsedTime >= YELLOW_INTERVAL) {
+                mElapsedTime -= YELLOW_INTERVAL;
+                mCurrentFrame = 12;
+                mNumFrames = 18;
+            }
+            break;
+        }
+
+        case 12: {
+            if (mElapsedTime >= RED_INTERVAL) {
+                mElapsedTime -= RED_INTERVAL;
+                mCurrentFrame = 0;
+                mNumFrames = 6;
+            }
+            break;
+        }
+
+        default: {
+            mCurrentFrame = (++mCurrentFrame) % mNumFrames;
+        }
+    }
+
+    sf::Vector2f spriteSize = mSprite.getLocalBounds().getSize();
+    sf::IntRect textureRect = sf::IntRect(
+        mCurrentFrame * spriteSize.x, 0, spriteSize.x, spriteSize.y
+    );
+
+    mSprite.setTextureRect(textureRect);
+}
 
 void TrafficLight::drawCurrent(
     sf::RenderTarget& target, sf::RenderStates states
 ) const {
     target.draw(mSprite, states);
 }
-
-void TrafficLight::updateMovementPattern(sf::Time dt) {}
