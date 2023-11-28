bedc061664a5962a3903f7c2af490d211b74675f
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-11-28 11:01:18 +0700

    add Motion

---
+++ src/Motion.cpp
@@ -0,0 +1,26 @@
+#include "Motion.hpp"
+
+#include <cassert>
+#include <cmath>
+
+using namespace Motion;
+
+float Linear::operator()(float x) {
+    assert(0 <= x && x <= 1);
+    return x;
+}
+
+float Quad::operator()(float x) {
+    assert(0 <= x && x <= 1);
+    return x * x;
+}
+
+float Bezier::operator()(float x) {
+    assert(0 <= x && x <= 1);
+    return x * x * (3.f - 2.f * x);
+}
+
+float Sigmoid::operator()(float x) {
+    assert(0 <= x && x <= 1);
+    return 1.f / (1.f + exp(-x));
+}
