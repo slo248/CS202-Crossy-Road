c6a2eb2053ce7f9976daac2510f28b78ccb33e4c
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-11-29 15:50:41 +0700

    add attributes in World

--- include/World.hpp
+++ include/World.hpp
@@ -1,6 +1,31 @@
 #ifndef WORLD_HPP
 #define WORLD_HPP
 
-class World {};
+#include <SFML/Graphics/RenderWindow.hpp>
+
+#include "Character.hpp"
+#include "CommandQueue.hpp"
+#include "ResourceIdentifiers.hpp"
+
+class World {
+   public:
+   private:
+    enum Layer { Background, OnGround, Air, LayerCount };
+
+   private:
+    TextureHolder& mTextures;
+    FontHolder& mFonts;
+    sf::RenderWindow& mWindow;
+
+    sf::View mWorldView;
+
+    SceneNode mSceneGraph;
+    std::array<SceneNode*, LayerCount> mLayers;
+
+    sf::Vector2f mWorldBounds;
+    Character* mPlayer;
+
+    CommandQueue mCommandQueue;
+};
 
 #endif
