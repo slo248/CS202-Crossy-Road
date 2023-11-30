c6a3f152b8b3499306513ffd00d90c4e95812199
zHuskyz <nvk01052004@gmail.com>
2023-11-30 11:24:28 +0700

    convert to 14x10 + redesign

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,19 @@
 #include "ResourceHolder.hpp"
 #include "World.hpp"
 
+/* #include "ChooseModeState.hpp"
+#include "Config.hpp"
+#include "CreditState.hpp"
+#include "LevelState.hpp"
+#include "LoseState.hpp"
+#include "MenuState.hpp"
+#include "PauseState.hpp"
+#include "RankingState.hpp"
+#include "SavedState.hpp"
+#include "SettingState.hpp"
+#include "State.hpp"
+#include "StateStack.hpp"
+#include "WinState.hpp" */
 
 int main() {
     srand(time(0));
@@ -29,7 +42,42 @@ int main() {
 
     sf::Clock clock;
     // sf::Time dt = sf::Time::Zero;
-    while (window.isOpen()) {
+    /* while (window.isOpen()) {
+    State::Context context(window, textures, fonts, player);
+    StateStack stack(context);
+    CreditState creditState(stack, context);
+    WinState winState(stack, context);
+    LoseState loseState(stack, context);
+    SettingState settingState(stack, context);
+    PauseState pauseState(stack, context);
+    MenuState menuState(stack, context);
+    RankingState rankingState(stack, context);
+    ChooseModeState chooseModeState(stack, context);
+
+    stack.registerState<SettingState>(States::Setting);
+    stack.registerState<CreditState>(States::Credit);
+    stack.registerState<WinState>(States::Win);
+    stack.registerState<LoseState>(States::Lose);
+    stack.registerState<PauseState>(States::Pause);
+    stack.registerState<MenuState>(States::Menu);
+    stack.registerState<RankingState>(States::Ranking);
+    stack.registerState<LevelState>(States::Level);
+    stack.registerState<SavedState>(States::Saved);
+    stack.registerState<ChooseModeState>(States::ChooseMode);
+
+    stack.pushState(States::Credit);
+    stack.pushState(States::Setting);
+    // stack.pushState(States::Win);
+    // stack.pushState(States::Lose);
+    stack.pushState(States::Pause);
+    stack.pushState(States::Level);
+    stack.pushState(States::Ranking);
+    stack.pushState(States::Saved);
+    stack.pushState(States::ChooseMode);
+    stack.pushState(States::Menu); */
+
+    while (stack.mContext.window->isOpen()) {
+        stack.mContext.window->clear(sf::Color::White);
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) window.close();
